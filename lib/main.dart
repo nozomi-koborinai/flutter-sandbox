18988ebdc6d6e26bfe9d7dde89e0a2acca140be5
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-10-08 22:24:02 +0900

    mainpageをstatefulからstatelessに変更

--- lib/main.dart
+++ lib/main.dart
@@ -26,48 +26,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Text(
+          'cloud functions test',
+          style: Theme.of(context).textTheme.headline4,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: callCloudFunction,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
+
+  /// ボタンタップに対するイベントハンドラ
+  void callCloudFunction() {}
 }
