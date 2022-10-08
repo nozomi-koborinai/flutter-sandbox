316ce7903a7fd1cfb18d0075720c7c6ba977d503
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-10-08 18:41:40 +0900

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/firebase_options.dart';
 
-void main() {
+void main() async {
+  // Firebaseの初期化
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
