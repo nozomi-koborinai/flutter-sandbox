1ab98434c0c7b1c00ec7dd4b72027063156ab154
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-11-21 21:35:31 +0900

    feat: 🎸 画像を少しずつ透明にするようにしてみる

--- lib/samples/stack/stack_item.dart
+++ lib/samples/stack/stack_item.dart
@@ -23,15 +23,30 @@ class _StackItemState extends State<StackItem> {
   // = 一枚後ろの画像が現れることになる
   bool _visible = true;
 
-  late Timer timer;
+  // 画像の透明度を保持する値
+  double _opacity = 1.0;
+
+  late Timer visibleTimer;
+  late Timer opacityTimer;
 
   @override
   void initState() {
     super.initState();
 
-    timer = Timer(Duration(milliseconds: widget.visibleMilliseconds), () {
+    // 画像が非表示になるまでの時間を設定
+    visibleTimer =
+        Timer(Duration(milliseconds: widget.visibleMilliseconds), () {
       setState(() {
         _visible = false;
+      });
+    });
+
+    // 画像が透明になり始めるまでの時間を設定
+    // ここを調整するといい感じになる？
+    opacityTimer = Timer(
+        Duration(milliseconds: widget.visibleMilliseconds * 0.7.toInt()), () {
+      setState(() {
+        _opacity = 0.0;
       });
     });
   }
@@ -39,19 +54,25 @@ class _StackItemState extends State<StackItem> {
   @override
   void dispose() {
     super.dispose();
-    timer.cancel();
+    // timerを破棄
+    visibleTimer.cancel();
+    opacityTimer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
       visible: _visible,
-      child: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(widget.imagePath),
-          fit: BoxFit.cover,
-        )),
+      child: AnimatedOpacity(
+        duration: const Duration(milliseconds: 1000), // 不透明 -> 透明にどれくらいの時間をかけるか
+        opacity: _opacity,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage(widget.imagePath),
+            fit: BoxFit.cover,
+          )),
+        ),
       ),
     );
   }
