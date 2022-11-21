761bea2593d1b423d9f3b79358330304cea71530
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-11-21 21:48:33 +0900

    feat: 🎸 いっそのことvisibleをやめてopacityだけで制御してみる

--- lib/samples/stack/stack_item.dart
+++ lib/samples/stack/stack_item.dart
@@ -18,33 +18,19 @@ class StackItem extends StatefulWidget {
 }
 
 class _StackItemState extends State<StackItem> {
-  // 画像を表示/非表示制御するためのフラグ
-  // 引数にて指定された時間が経過するとfalseになり非表示になる
-  // = 一枚後ろの画像が現れることになる
-  bool _visible = true;
-
   // 画像の透明度を保持する値
   double _opacity = 1.0;
 
-  late Timer visibleTimer;
   late Timer opacityTimer;
 
   @override
   void initState() {
     super.initState();
 
-    // 画像が非表示になるまでの時間を設定
-    visibleTimer =
-        Timer(Duration(milliseconds: widget.visibleMilliseconds), () {
-      setState(() {
-        _visible = false;
-      });
-    });
-
     // 画像が透明になり始めるまでの時間を設定
     // ここを調整するといい感じになる？
-    opacityTimer = Timer(
-        Duration(milliseconds: widget.visibleMilliseconds * 0.7.toInt()), () {
+    opacityTimer =
+        Timer(Duration(milliseconds: widget.visibleMilliseconds), () {
       setState(() {
         _opacity = 0.0;
       });
@@ -55,24 +41,20 @@ class _StackItemState extends State<StackItem> {
   void dispose() {
     super.dispose();
     // timerを破棄
-    visibleTimer.cancel();
     opacityTimer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Visibility(
-      visible: _visible,
-      child: AnimatedOpacity(
-        duration: const Duration(milliseconds: 1000), // 不透明 -> 透明にどれくらいの時間をかけるか
-        opacity: _opacity,
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage(widget.imagePath),
-            fit: BoxFit.cover,
-          )),
-        ),
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 1000), // 不透明 -> 透明にどれくらいの時間をかけるか
+      opacity: _opacity,
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(widget.imagePath),
+          fit: BoxFit.cover,
+        )),
       ),
     );
   }
