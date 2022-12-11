9c0c600e594ca0159c462fc51d5ff4054ed74f6e
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-12-11 13:57:18 +0900

    ♻️ refactor: レイアウト変更

--- lib/samples/command_design_pattern/widget/page/command_example_page.dart
+++ lib/samples/command_design_pattern/widget/page/command_example_page.dart
@@ -7,7 +7,6 @@ import '../../command/command.dart';
 import '../../command/command_history.dart';
 import '../component/command_history_column.dart';
 import '../component/shape.dart';
-import '../component/shape_container.dart';
 
 class CommandExamplePage extends StatefulWidget {
   const CommandExamplePage({super.key});
@@ -43,6 +42,7 @@ class _CommandExamplePageState extends State<CommandExamplePage> {
   }
 
   void _undo() {
+    if (_commandHistory.isEmpty) return;
     setState(() {
       _commandHistory.undo();
     });
@@ -52,31 +52,56 @@ class _CommandExamplePageState extends State<CommandExamplePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        'Command Design Pattern',
-        style: TextStyle(color: Colors.grey[700]),
-      )),
+        title: Text(
+          'Command Design Pattern',
+          style: TextStyle(color: Colors.grey[700]),
+        ),
+      ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(
           horizontal: 10,
         ),
         child: Column(
           children: <Widget>[
-            ShapeContainer(
-              shape: _shape,
+            SizedBox(
+              height: 160.0,
+              child: Center(
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 500),
+                  height: _shape.height,
+                  width: _shape.width,
+                  decoration: BoxDecoration(
+                    color: _shape.color,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: const Icon(
+                    Icons.star,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 15),
             ElevatedButton(
-                onPressed: _changeColor, child: const Text('Change color')),
+              onPressed: _changeColor,
+              child: const Text('Change color command'),
+            ),
+            const SizedBox(height: 5),
             ElevatedButton(
-                onPressed: _changeHeight, child: const Text('Change height')),
+              onPressed: _changeHeight,
+              child: const Text('Change height command'),
+            ),
+            const SizedBox(height: 5),
             ElevatedButton(
-                onPressed: _changeWidth, child: const Text('Change width')),
+              onPressed: _changeWidth,
+              child: const Text('Change width command'),
+            ),
             const Divider(),
             ElevatedButton(
-                onPressed: _commandHistory.isEmpty ? null : _undo,
-                child: const Text('Undo')),
-            const SizedBox(height: 20),
+              onPressed: _undo,
+              child: const Text('Undo'),
+            ),
+            const SizedBox(height: 15),
             CommandHistoryColumn(
               commandList: _commandHistory.commandHistoryList,
             ),
