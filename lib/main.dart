0e72720557bb666865e20db39ed07ffee7bdae4c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-02-09 16:22:21 +0900

    🚀 feat: デバッグモード時のデバッグラベルを削除

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Sand Box',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Colors.transparent),
         useMaterial3: true,
